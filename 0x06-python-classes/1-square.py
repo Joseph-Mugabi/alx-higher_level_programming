edf783b08c97c77de258d903fd6927f9fbec8ed1
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-10-11 17:25:34 +0000

    class square module

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,17 @@
 
 
 class Square:
+    """
+    class Square definition
+
+    Args:
+        size : size of a side in square
+    """
     def __init__(self, size):
+        """
+        Initializes square
+
+        Attributes:
+            size: size of a side of square
+        """
         self.__size = size
