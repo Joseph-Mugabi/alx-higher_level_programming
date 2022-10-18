f6738cffd7dfe2404f21ff0750a997de21fcd773
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-10-18 19:50:12 +0000

    Clear strings

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+101-locked_class Module
+"""
+
+
 class LockedClass:
     """A locked class that only lets the user dynamically create the instance
     attribute 'first_name'"""
