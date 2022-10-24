f89387dc3f03e8d793c8f0ae9dd7263bf7953004
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2022-10-24 17:58:35 +0000

    inheritence

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+class MyList(list):
+    """A subclass of list"""
+    def __init__(self):
+        """Initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains the function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """return true if the obj is the exactly an instance of
+    the specified class , otherwise false"""
+    return (type(obj) == a_class)
